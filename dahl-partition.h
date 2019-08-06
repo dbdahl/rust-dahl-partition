2627295b5eca2f872e294d68ea6a29639a9339ed
David B. Dahl <dahl@stat.byu.edu>
2019-08-06 06:27:50 -0600

    Rename variable

--- dahl-partition.h
+++ dahl-partition.h
@@ -3,19 +3,22 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-void dahl_partition__distribution__crp__sample(int n_samples, int n_items, double mass, int *ptr);
+void dahl_partition__distribution__crp__sample(int n_partitions,
+                                               int n_items,
+                                               double mass,
+                                               int *ptr);
 
-void dahl_partition__summary__expected_loss(int n_samples,
+void dahl_partition__summary__expected_loss(int n_partitions,
                                             int n_items,
-                                            const int *partition_ptr,
+                                            int *partition_ptr,
                                             double *psm_ptr,
                                             int loss,
                                             double *results_ptr);
 
-void dahl_partition__summary__psm(int n_samples,
+void dahl_partition__summary__psm(int n_partitions,
                                   int n_items,
                                   int parallel,
-                                  const int *partitions_ptr,
+                                  int *partitions_ptr,
                                   double *psm_ptr);
 
 double dahl_partition__utils__lbell(int n);
